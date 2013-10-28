85e5867812e4a5d126c962eda8ab66de6106bde9
Ben Nelson <nelson.ben.c@gmail.com>
2013-10-27 21:53:55 -0600

    starting out new rails project in rails 3.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SigmaNew::Application.config.secret_token = 'e8fc3f2a252494fa41246b5b061195bcf3c7671881d82d06f91627cb1a2e74f946c82ae69f7de3e0112253ef1682cde1cc521dc9751a68ce25a01977c249fdef'
