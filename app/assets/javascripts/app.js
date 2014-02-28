deb788de5b56bf715034cf16e4a6ad55b68fec0a
Ben Nelson <nelson.ben.c@gmail.com>
2014-02-28 15:21:29 -0700

    adding in angular stuff and basic bower scaffolding

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('sigmaNuApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+        //TODO: find out how to do routing on both ends, kind of a bastard application
+    $routeProvider
+      .when('/', {
+            //TODO: will this work? dunno if erb works with it
+        templateUrl: '../../views/home/index.html.erb',
+        controller: 'MainCtrl'
+      });
+  });
