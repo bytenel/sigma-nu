2751ee7f5c10f070a91c139e7238f3ac0a79a836
Ben Nelson <nelson.ben.c@gmail.com>
2014-03-17 20:22:46 -0600

    heroku requires you to not initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module SigmaNew
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku has a strong seperation of building and running
+    config.assets.initialize_on_precompile = false
   end
 end
