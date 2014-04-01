f9450c75892598bcac2563e6817c93feae6ff01c
Ben Nelson <nelson.ben.c@gmail.com>
2014-03-31 21:32:58 -0600

    adding in decorator for article show view

--- app/decorators/article_decorator.rb
+++ app/decorators/article_decorator.rb
@@ -10,4 +10,27 @@ class ArticleDecorator < Draper::Decorator
   #     end
   #   end
 
+  def flag_string
+    return flag ? "Unflag" : "Flag"
+  end
+
+  def inverted_flag
+	return (!flag).to_s
+  end
+
+  def formatted_posts
+  	formatted_posts = ""
+  	posts.each do |post| 
+	    if post.body.nil?
+	      next 
+	    end 
+
+	  formatted_posts += 
+	  "<br />
+	  #{ post.user.username }  :   #{ post.created_at } 
+	  <br/>
+	  #{ post.body }" 
+	 end 
+   return formatted_posts
+  end
 end
