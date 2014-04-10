6f0e30d666b1e75732cab28dd4245dc7335a4b88
Ben Nelson <nelson.ben.c@gmail.com>
2014-04-09 23:07:50 -0600

    getting active navbar fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def isActive?(controller, action)
+		if params[:action] == action && params[:controller] == controller || (params[:controller] == 'articles' && action == 'articles' || params[:controller] == 'topics' && action == 'any' || params[:controller] == 'forums' && action == 'any')
+		   return "active"
+		else
+			return ""
+	    end
+	end
+
 end
