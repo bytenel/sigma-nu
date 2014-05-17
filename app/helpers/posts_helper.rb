3ad2c3309395a10e5709704806d9a7028695fffb
Ben Nelson <nelson.ben.c@gmail.com>
2014-05-16 19:13:25 -0600

    fixing some things with showing buttons in topics

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -18,7 +18,7 @@ module PostsHelper
 	                           #{link_to "Reply", new_post_post_path(post)}
 	                           #{link_to "Quote", new_topic_post_path(@topic, :quote => post)}"
 
-	            if current_user.posts.includes(post)
+	            if current_user.posts.include?(post)
 	                final_nodes += "#{link_to "Edit", edit_post_path(post)}
 	                           #{button_to "Delete", post, html_options = 
 	                           {:class => "btn btn-mini btn-danger", :confirm => "Are you sure?", :method => :delete}}"
