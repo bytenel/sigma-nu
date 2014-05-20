fa06d47da05cf07e9eae09b218b9f8b04d11baab
Ben Nelson <nelson.ben.c@gmail.com>
2014-05-19 23:01:56 -0600

    fixing button links on posts to not be anchor tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -12,14 +12,25 @@ module PostsHelper
 	                        <span class='avatar'><!-- This space reserved for Avatar --></span>
 	                        <span class='left post_date smaller'>#{post.created_at}</span>
 	                        <span class='right controls'>
+	                        <div class='btn-group'>
+	                           <button class='btn btn-primary'>
 	                           #{link_to post.flagged_string, 
 	                           {:controller => 'posts', :action => 'update', :post => {:flag => !post.flag }}, 
 	                           html_options = {:method => :put }}
-	                           #{link_to "Reply", new_post_post_path(post)}
-	                           #{link_to "Quote", new_topic_post_path(@topic, :quote => post)}"
-
+	                           </button>
+	                           <button class='btn btn-primary'>
+		                           #{link_to "Reply", new_post_post_path(post)}
+	                           </button>
+	                           <button class='btn btn-primary'>
+		                           #{link_to "Quote", new_topic_post_path(@topic, :quote => post)}
+		                       </button>
+		                    </div>
+		                           "
 	            if current_user.posts.include?(post)
-	                final_nodes += "#{link_to "Edit", edit_post_path(post)}
+	                final_nodes += "
+	                           <button class='btn btn-primary'>
+					             #{link_to "Edit", edit_post_path(post)}
+					           </button>
 	                           #{button_to "Delete", post, html_options = 
 	                           {:class => "btn btn-mini btn-danger", :confirm => "Are you sure?", :method => :delete}}"
                 end
