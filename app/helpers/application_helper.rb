a4f2295831c9feed74e0acee6efa21f7416cb7e2
Ben Nelson <nelson.ben.c@gmail.com>
2014-06-02 21:00:00 -0600

    cleaning up an if statement and adding a todo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
-
+#TODO: refactor this
 	def isActive?(controller, action)
-		if params[:action] == action && params[:controller] == controller || (params[:controller] == 'articles' && action == 'articles' || params[:controller] == 'topics' && action == 'any' || params[:controller] == 'forums' && action == 'any')
+		if params[:action] == action && params[:controller] == controller || \
+			(params[:controller] == 'articles' && action == 'articles' || \
+				params[:controller] == 'topics' && action == 'any' || \
+				params[:controller] == 'forums' && action == 'any')
 		   return "active"
 		else
 			return ""
